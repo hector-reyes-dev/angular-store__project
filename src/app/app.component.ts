916457b279cbfeee4d232ce95a62b93bb69eaf72
Hector <hectordevx@gmail.com>
2022-07-18 17:12:08 -0500

    feat(auth): Auth, Token and Users Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   imgParent = '';
   showImg = true;
+  token = '';
+  userEmail = '';
+
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   onLoaded(img: string) {
     console.log('Log padre', img);
@@ -15,5 +24,31 @@ export class AppComponent {
 
   toogleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService
+      .create({
+        name: 'Pablo',
+        email: 'pablo@email.com',
+        password: '1221',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
+  }
+
+  login() {
+    this.authService.login('pablo@email.com', '1221').subscribe((rta) => {
+      console.log(rta.access_token);
+      this.token = rta.access_token;
+    });
+  }
+
+  getProfile() {
+    this.authService.profile(this.token).subscribe((profile) => {
+      console.log(profile.email);
+      this.userEmail = profile.email;
+    });
   }
 }
