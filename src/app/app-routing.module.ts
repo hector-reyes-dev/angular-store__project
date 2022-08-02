328dc4dbb9b8cead852cbfbfe02d2a97c91b986e
Hector <hectordevx@gmail.com>
2022-08-02 17:26:38 -0500

    feat(product-detail): Component, template and styles. Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
   {
     path: 'home',
     component: HomeComponent,
@@ -18,6 +24,10 @@ const routes: Routes = [
   {
     path: 'category/:id',
     component: CategoryComponent,
+  },
+  {
+    path: 'product/:id',
+    component: ProductDetailComponent,
   },
   {
     path: 'my-cart',
