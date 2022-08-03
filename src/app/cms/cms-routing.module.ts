64bc347fec1453801e7e1bb4b085e83427ba30dc
Hector <hectordevx@gmail.com>
2022-08-03 17:56:00 -0500

    feat(cms): CMS First commit

--- src/app/cms/cms-routing.module.ts
+++ src/app/cms/cms-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { GridComponent } from './pages/grid/grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'grid',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'grid',
+        component: GridComponent,
+      },
+      {
+        path: 'tasks',
+        component: TasksComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CmsRoutingModule { }
+export class CmsRoutingModule {}
